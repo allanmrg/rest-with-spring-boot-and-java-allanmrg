3f71ca2c080a7767f67a03d6bfa98d7ac03ca53a
Allan Goncalves <allandbz@hotmail.com>
2024-05-20 15:35:38 +0200

    init maven project

---
+++ rest-with-spring-boot-and-java-allanmrg/src/main/java/com/allanmrg/restwithspringbootandjavaallanmrg/RestWithSpringBootAndJavaAllanmrgApplication.java
@@ -0,0 +1,13 @@
+package com.allanmrg.restwithspringbootandjavaallanmrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaAllanmrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaAllanmrgApplication.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java-allanmrg/src/test/java/com/allanmrg/restwithspringbootandjavaallanmrg/RestWithSpringBootAndJavaAllanmrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allanmrg.restwithspringbootandjavaallanmrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaAllanmrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
